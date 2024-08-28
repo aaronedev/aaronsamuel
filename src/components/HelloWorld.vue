a45a0004df668bff8f1a78fc8650ded88dfee8da
aaronedev <code@aar.one>
2024-08-28 18:02:14 +0200

    -m tested tailwindcss and it works

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -23,7 +23,15 @@ const count = ref(0);
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
-
+	<div class="text-8xl p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+		<div class="shrink-0">
+			<img class="size-12" src="" alt="ChitChat Logo">
+		</div>
+		<div>
+			<div class="text-xl font-medium text-black">ChitChat</div>
+			<p class="text-slate-500">You have a new message!</p>
+		</div>
+	</div>
   <p>
     Check out
     <a
