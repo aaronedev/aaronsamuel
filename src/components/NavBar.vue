b42e6179cd5017897d6a44399fbc549fea1dbb74
aaronedev <code@aar.one>
2024-09-02 18:10:54 +0200

    -m added navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,62 +1,56 @@
 <template>
-  <nav
-    class="fixed left-0 top-0 z-50 w-full bg-slate-950 text-text-primary shadow-lg"
-  >
-    <div class="container mx-auto flex items-center justify-between p-4">
-      <!-- Logo or Brand Name -->
-      <div class="text-xl font-bold">
-        <a
-          href="#"
-          class="text-accent-blue hover:underline"
-        >My Portfolio</a>
-      </div>
+	<nav
+		class="fixed left-0 top-0 z-50 w-full bg-slate-950 text-text-primary shadow-lg"
+	>
+		<div class="container mx-auto flex items-center justify-between p-4">
+			<!-- Logo or Brand Name -->
+			<div class="text-xl font-bold">
+				<a href="#" class="text-accent-blue hover:underline">My Portfolio</a>
+			</div>
 
-      <!-- Icons and Toggle -->
-      <div class="flex items-center space-x-4">
-        <!-- FontAwesome Icons -->
-        <a
-          href="https://github.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="text-lg text-accent-blue hover:text-accent-purple"
-        >
-          <i class="fab fa-github" />
-        </a>
-        <a
-          href="https://www.linkedin.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="text-lg text-accent-blue hover:text-accent-purple"
-        >
-          <i class="fab fa-linkedin" />
-        </a>
+			<!-- Icons and Toggle -->
+			<div class="flex items-center space-x-4">
+				<!-- FontAwesome Icons -->
+				<a
+					href="https://github.com"
+					target="_blank"
+					rel="noopener noreferrer"
+					class="text-lg text-accent-blue hover:text-accent-purple"
+				>
+					<i class="fab fa-github" />
+				</a>
+				<a
+					href="https://www.linkedin.com"
+					target="_blank"
+					rel="noopener noreferrer"
+					class="text-lg text-accent-blue hover:text-accent-purple"
+				>
+					<i class="fab fa-linkedin" />
+				</a>
 
-        <!-- Toggle Switch for Light/Dark Mode -->
-        <div>
-          <label
-            for="themeToggle"
-            class="flex cursor-pointer items-center"
-          >
-            <div class="relative flex items-center">
-              <input
-                id="themeToggle"
-                type="checkbox"
-                class="sr-only"
-                @change="toggleTheme"
-              >
-              <div
-                class="flex h-6 w-10 items-center rounded-full bg-gray-400 shadow-inner"
-              />
-              <div
-                class="dot absolute left-1 top-1 h-4 w-4 rounded-full bg-white transition"
-              />
-            </div>
-            <span class="ml-3 text-sm">Dark Mode</span>
-          </label>
-        </div>
-      </div>
-    </div>
-  </nav>
+				<!-- Toggle Switch for Light/Dark Mode -->
+				<div>
+					<label for="themeToggle" class="flex cursor-pointer items-center">
+						<div class="relative flex items-center">
+							<input
+								id="themeToggle"
+								type="checkbox"
+								class="sr-only"
+								@change="toggleTheme"
+							/>
+							<div
+								class="flex h-6 w-10 items-center rounded-full bg-gray-400 shadow-inner"
+							/>
+							<div
+								class="dot absolute left-1 top-1 h-4 w-4 rounded-full bg-white transition"
+							/>
+						</div>
+						<span class="ml-3 text-sm">Dark Mode</span>
+					</label>
+				</div>
+			</div>
+		</div>
+	</nav>
 </template>
 
 <script>
